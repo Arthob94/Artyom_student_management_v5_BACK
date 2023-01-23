e261156daf3656396b8fa64fccd6f5f64e9bc76f
Amir Hauz <amirhauz@Amirs-MacBook-Pro.local>
2023-01-23 16:31:52 +0200

    first commit

---
+++ Readme.md
@@ -0,0 +1,12 @@
+# flask back with file.json
+## contains:
+### Crud without delete 
+### CORS
+
+
+
+
+# Run:
+    create a virtual enviroment
+    pip install -r .\requirements.txt
+    python app.py
---
+++ app.py
@@ -0,0 +1,68 @@
+import json
+
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+MY_FILE= 'my_student_list.json'
+
+def load_data():
+    with open(MY_FILE) as f:    #open file for reading
+        return json.load(f) 
+
+@app.route('/student', methods=['POST'])
+def create_student():
+    data = request.get_json()   #request data as dict
+    existing_data_array = load_data()   # 
+    existing_data_array.append(data)
+    with open(MY_FILE, 'w') as f:
+        f.write(json.dumps(existing_data_array))  # json.dumps converts to a string and f.write writes the string into a file.
+
+    return data
+
+@ app.route('/student', methods=['GET'])
+@ app.route('/student/<string:student_email>', methods=['GET'])
+def read_worker(student_email=""):
+    json_data = load_data()
+    if (student_email == ""):
+        return json_data
+    else:
+        for x in json_data:
+            if x['semail'] == student_email:
+                return [x]
+        return [{
+            "semail": "No such email",
+            "sname": "",
+            "gmathematics": "",
+            "gcomputers": "",
+            "genglish": ""
+        }]
+
+@app.route('/student/<string:student_email>', methods=['PUT'])
+def update_worker(student_email):
+    json_data = load_data()
+    input_data = request.get_json()   
+    student_found = False
+    for student in json_data:
+        if (student_email == student['semail']):
+            student_found = True
+            student.update(input_data)
+            break
+    if not student_found: 
+        return {"msg": "no such student "}
+    with open(MY_FILE, 'w') as f:
+        f.write(json.dumps(json_data))
+
+    
+    return input_data
+    
+
+
+
+
+
+if __name__ == '__main__':
+    with app.app_context():
+        app.run(debug=True)
